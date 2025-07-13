987dcfb306983dcfefac0019854a3a3801f17be9
basantbht <bhattbasant012@gmail.com>
2025-07-13 12:16:37 +0545

    create user and product models

---
+++ backend/config/mongodb.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    
+    try {
+
+        mongoose.connection.on("connected" , () => {
+            console.log("Database Connected");
+        })
+        await mongoose.connect(`${process.env.MONGODB_URI}/ecommerce`);
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import cors from 'cors' 
 import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
 
 // App Config
 const app = express();
 const PORT = process.env.PORT || 8000;
+connectDB();
+connectCloudinary();
 
 // Middlewares
 app.use(express.json());
