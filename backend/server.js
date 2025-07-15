e310319c2c236e5852bc5027fc70bd3fdd0aae14
basantbht <bhattbasant012@gmail.com>
2025-07-15 14:00:17 +0545

    product added, producted removed, cloudinary implemented for image upload

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRoute.js';
+import productRouter from './routes/productRoute.js';
 
 // App Config
 const app = express();
@@ -15,9 +17,13 @@ app.use(express.json());
 app.use(cors());``
 
 // Api endpoints
+app.use("/api/user",userRouter);
+app.use("/api/product",productRouter);
+
 app.get("/", (req,res) => {
     res.send("API Working");
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
